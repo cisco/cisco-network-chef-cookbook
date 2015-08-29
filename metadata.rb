f1389247a14d47a32737f819f4b7729853262d39
Matt Ray <matthewhray@gmail.com>
2015-08-29 17:05:25 -0500

    restore maintainer_email

--- metadata.rb
+++ metadata.rb
@@ -16,6 +16,7 @@
 
 name             'cisco-cookbook'
 maintainer       'Cisco Systems'
+maintainer_email 'chef-agent-dev@cisco.com'
 license          'Apache v2.0'
 description      'Installs/Configures Cisco NX-OS devices'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
