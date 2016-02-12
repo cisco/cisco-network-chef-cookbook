f9540529f5aa91dde14c6ba041f7eaa37630632d
Rob Gries <robgrie@cisco.com>
2016-02-12 12:05:29 -0500

    Destroy sub-interface before cleaning parent intf
    If parent interface is cleaned before sub-interface is destroyed,
    CLI error occurs because the mtu value of the parent interface
    must be greater than or equal to the sub-interface's mtu.

--- recipes/demo_cleanup.rb
+++ recipes/demo_cleanup.rb
@@ -64,12 +64,13 @@ cisco_interface 'loopback2' do
   action :destroy
 end
 
-cisco_interface 'Ethernet1/1' do
-  switchport_mode 'default'
-end
-
+# destroy sub-if first because of mtu dependency
 cisco_interface 'Ethernet1/1.1' do
   action :destroy
+end
+
+cisco_interface 'Ethernet1/1' do
+  switchport_mode 'default'
 end
 
 cisco_interface 'Ethernet1/3' do
