ff699e9557fba34bf6b8ffe644c89790bd8b6c42
rtpfe1 <rtpfe1@ansible-server-1.cisco.com>
2017-04-03 09:34:11 -0400

    Delete old version of node_utils

--- files/default/vendor/gems/cisco_node_utils-1.3.0/spec/client_spec.rb
+++
@@ -1,7 +0,0 @@
-require_relative 'spec_helper.rb'
-require 'shared_examples_for_clients'
-require 'cisco_node_utils/client'
-
-describe Cisco::Client do
-  it_behaves_like 'all clients'
-end
--- files/default/vendor/gems/cisco_node_utils-1.3.0/spec/isolate/all_clients_spec.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../spec_helper.rb'
-
-context 'when both clients are installed' do
-  it 'should have both clients' do
-    require 'cisco_node_utils'
-    expect(Cisco::Client.clients).to eql [Cisco::Client::NXAPI,
-                                          Cisco::Client::GRPC]
-  end
-end
